f75a0ac78009d8da6d9f123c6fe97f0ff667919c
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-07-17 11:38:01 +0900

    wrote application logic. only Amethyst can access file.

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import re
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def is_supporting_client(uagent, referer):
+    if uagent.find("Amethyst") != -1:
+        return True
+    else:
+        return False
+
+def check_path(path):
+    pattern = r'^[a-zA-Z0-9/\.]*$'
+    if re.match(pattern, path):
+        return True
+    else:
+        return False
+
+@app.route('/<path:path>')
+def root_path(path):
+    uagent = request.headers.get('User-Agent')
+    if not is_supporting_client(uagent, request.referrer):
+        return "", 403
+    
+    if not check_path(path):
+        return "", 400
+    
+    return redirect("https://nostr.build/" + path)
