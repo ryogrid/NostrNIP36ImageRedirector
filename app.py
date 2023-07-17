c27a7041ddf890b6daae521db4316eab7f436bf0
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-07-17 15:17:20 +0900

    itroduced cookie.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 import re
 
 app = Flask(__name__)
@@ -9,7 +9,14 @@ SUPPORTING_WEB_CLIENTS = [
     "nostter.vercel.app",
     "astraea.mousedev.page",    
 ]
-def is_supporting_client(uagent, referer):
+def is_supporting_client(uagent, referer, cookies):
+    try:
+        # when a web client already permitted on the same browser
+        if cookies.get('suppport36') == "true":
+            return True
+    except:
+        pass
+
     if uagent.find("okhttp/5.0.0-alpha.11") != -1: # Amethyst
         # want to add Plebstar... but user agent is unknown...
         return True
@@ -30,10 +37,12 @@ def check_path(path):
 @app.route('/<path:path>')
 def root_path(path):
     uagent = request.headers.get('User-Agent')
-    if not is_supporting_client(uagent, request.referrer):
+    if not is_supporting_client(uagent, request.referrer, request.cookies):
         return "", 403
     
     if not check_path(path):
         return "", 400
     
-    return redirect("https://nostr.build/" + path)
+    response = make_response(redirect("https://nostr.build/" + path))
+    response.set_cookie('suppport36', "true")
+    return response
